3be5824d9c80e60c3502fb4372e3e5feae68f3a5
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2020-11-13 16:51:56 +0100

    Porting to zephyr RTOS.

--- src/source/TMC2130Stepper_MACROS.h
+++ src/source/TMC2130Stepper_MACROS.h
@@ -1,22 +1,29 @@
 #ifndef TMC2130Stepper_MACROS_H
 #define TMC2130Stepper_MACROS_H
-#include "TMC2130Stepper.h"
 #include "../TMC2130Stepper_REGDEFS.h"
+#include "TMC2130Stepper.h"
 
-#define TMC_WRITE_REG(R) 	send2130(TMC2130_WRITE|REG_##R, &R##_sr);
+// TODO for Gods's sake, it's 2020. Rewrite to some constexpr/template functions.
 
-#define TMC_READ_REG(R)   	send2130(TMC2130_READ|REG_##R, &R##_sr); return R##_sr
+#define TMC_WRITE_REG(R) writeCmd (TMC2130_WRITE | REG_##R, R##_sr);
 
-#define TMC_READ_REG_R(R)   tmp_sr=0; send2130(TMC2130_READ|REG_##R, &tmp_sr); return tmp_sr;
+// TODO check the status.
+#define TMC_READ_REG(R)                                                                                                                         \
+        R##_sr = readCmd (TMC2130_READ | REG_##R).first;                                                                                        \
+        return R##_sr
 
-#define TMC_MOD_REG(REG, SETTING) 	REG##_sr &= ~SETTING##_bm; \
-								REG##_sr |= ((uint32_t)B<<SETTING##_bp)&SETTING##_bm; \
-								TMC_WRITE_REG(REG);
+// TODO check the status.
+#define TMC_READ_REG_R(R) return readCmd (TMC2130_READ | REG_##R).first;
 
-#define TMC_GET_BYTE(REG, SETTING) 	return (REG()&SETTING##_bm) >> SETTING##_bp;
+#define TMC_MOD_REG(REG, SETTING)                                                                                                               \
+        REG##_sr &= ~SETTING##_bm;                                                                                                              \
+        REG##_sr |= ((uint32_t)B << SETTING##_bp) & SETTING##_bm;                                                                               \
+        TMC_WRITE_REG (REG);
 
-#define TMC_GET_BYTE_R(REG, SETTING) return (REG()&SETTING##_bm) >> SETTING##_bp;
+#define TMC_GET_BYTE(REG, SETTING) return (REG () & SETTING##_bm) >> SETTING##_bp;
 
-#define TMC_GET_BIT(REG, SETTING) 	return (bool)((REG()&SETTING##_bm) >> SETTING##_bp);
+#define TMC_GET_BYTE_R(REG, SETTING) return (REG () & SETTING##_bm) >> SETTING##_bp;
+
+#define TMC_GET_BIT(REG, SETTING) return (bool)((REG () & SETTING##_bm) >> SETTING##_bp);
 
 #endif
